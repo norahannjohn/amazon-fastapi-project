30d8bd022e385ba57ffd5a96d7cfecef1fd68c9d
norahannjohn <2411norah@gmail.com>
2026-06-10 16:50:03 +0530

    refactor: centralize application configuration

--- database.py
+++ database.py
@@ -1,15 +1,14 @@
-from dotenv import load_dotenv
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+from config.settings import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(settings.DATABASE_URL)
 
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 Base = declarative_base()
